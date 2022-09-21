ff6912914cbbd8458ada3fdf6762626efdf1404a
Sakib Rahman <seu.sakib@gmail.com>
2022-09-21 23:30:00 +0600

    add table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Categories from "./Components/Categories";
 
 function App() {
   return (
-    <div className="App">
-    
+    <div>
+      <h1 className="text-6xl">Dua Page</h1>
+      <div className="grid grid-cols-2">
+        <Categories />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Categories.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Categories() {
+  return (
+    <div className="overflow-x-auto">
+      <table className="table table-compact w-full">
+        <caption>Catagories</caption>
+        <tbody>
+          <tr>
+            <td>Content hear</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
